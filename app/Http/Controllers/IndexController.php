f16224a1d3417ddb191aa6978d4243b64dd9c524
wueryi <jishuai1274908713@qq.com>
2020-04-15 10:51:01 +0800

    增加冷数据缓存

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,57 +14,72 @@ use App\Models\Experience;
 use App\Models\Info;
 use App\Models\Skill;
 use App\Models\Work;
+use Illuminate\Support\Facades\Cache;
 
 class IndexController extends Controller
 {
+    const SECOND = 3600*24;
     public function getSkill()
     {
-        $Skill = Skill::query()
-            ->where('status', Skill::STATUS_ON)
-            ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
-            ->select(["brief", "content", "keyword"])
-            ->get();
+        $Skill = Cache::remember("skill",self::SECOND,function (){
+            return Skill::query()
+                ->where('status', Skill::STATUS_ON)
+                ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
+                ->select(["brief", "content", "keyword"])
+                ->get();
+        });
         return $this->respondWithSuccess($Skill);
     }
 
     public function getWork()
     {
-        $Work = Work::query()
-            ->where('status', Work::STATUS_ON)
-            ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
-            ->select(["company", "position", "responsibility", "begin_at", "end_at"])
-            ->get();
+        $Work = Cache::remember("work",self::SECOND,function (){
+            return $Work = Work::query()
+                ->where('status', Work::STATUS_ON)
+                ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
+                ->select(["company", "position", "responsibility", "begin_at", "end_at"])
+                ->get();
+        });
         return $this->respondWithSuccess($Work);
     }
 
     public function getExperience()
     {
-        $Experience = Experience::query()
-            ->where('status', Experience::STATUS_ON)
-            ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
-            ->select(["name", "brief", "skill", "responsibility", "difficulty", "achievement", "begin_at", "end_at", "image"])
-            ->get();
+        $Experience = Cache::remember("experience", self::SECOND, function (){
+            return Experience::query()
+                ->where('status', Experience::STATUS_ON)
+                ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
+                ->select(["name", "brief", "skill", "responsibility", "difficulty", "achievement", "begin_at", "end_at", "image"])
+                ->get();
+        });
         return $this->respondWithSuccess($Experience);
     }
 
     public function getEvaluate()
     {
-        $Evaluate = Evaluate::query()
-            ->where('status', Evaluate::STATUS_ON)
-            ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
-            ->select(["content"])
-            ->get();
+
+        $Evaluate = Cache::remember("evaluate", self::SECOND, function (){
+            return Evaluate::query()
+                ->where('status', Evaluate::STATUS_ON)
+                ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")
+                ->select(["content"])
+                ->get();
+        });
         return $this->respondWithSuccess($Evaluate);
     }
 
     public function getInfo(){
-        $Info = Info::query()
-            ->where('status', Evaluate::STATUS_ON)
-            ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")->select(["name", "key", "value"])->get()->toArray();
-        $data = [];
-        foreach($Info as $key => $value){
-            $data[$value['key']] = $value;
-        }
+        $data = Cache::remember("info", self::SECOND, function (){
+            $Info = Info::query()
+                ->where('status', Evaluate::STATUS_ON)
+                ->orderBy("sort", "ASC")->orderBy("created_at", "DESC")->select(["name", "key", "value"])->get()->toArray();
+            $data = [];
+            foreach($Info as $key => $value){
+                $data[$value['key']] = $value;
+            }
+            return $data;
+        });
+
         return $this->respondWithSuccess($data);
     }
 }
